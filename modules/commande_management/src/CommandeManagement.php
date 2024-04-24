679e9201393c1635c06f862befe7014589840563
Miandrilala <miandrilala@Miandrilalas-MacBook-Pro.local>
2024-04-24 11:28:09 +0300

    fix bug save commande

--- modules/commande_management/src/CommandeManagement.php
+++ modules/commande_management/src/CommandeManagement.php
@@ -19,31 +19,35 @@ class CommandeManagement
             $service = \Drupal::service('drupal.helper');
             $nid = $service->node->getLatestNodeId();
             $params = $service->helper->get_parameter();
-            $arrayData = json_decode($params["data"], TRUE);
-            $fields['title'] = "COM-".$nid  ;
-
-            $date_obj = new DrupalDateTime();
-            $date = $date_obj->format('Y-m-d');
-            $fields['field_date'] =  $date ; 
-            $fields['field_total_vente'] = $arrayData['total'] ;
-
-            if(isset($params['client'])){
-               $fields['field_client'] = $params['client'] ;
-            }
-            $fields['field_status'] = "unpayed" ;    
-            $items = [];
-            foreach($arrayData['items'] as $key => $item){    
-            // $article = \Drupal::service('entity_parser.manager')->node_parser( $item["id"]);
-            $items[$key] = [
-               'field_article' => $item["id"],
-               'field_quantite' => $item["qte"]
-            ];
-            }
-            $fields['field_articles'] = $items;
-            $com_new = \Drupal::service('crud')->save('node', 'commande', $fields);
-            if(is_object($com_new )){
-               return $com_new->id();
-            }
+            if(isset($params["data"])){
+               $arrayData = json_decode($params["data"], TRUE);
+               $fields['title'] = "COM-".$nid  ;
+   
+               $date_obj = new DrupalDateTime();
+               $date = $date_obj->format('Y-m-d');
+               $fields['field_date'] =  $date ; 
+               $fields['field_total_vente'] = $arrayData['total'] ;
+   
+               if(isset($params['client'])){
+                  $fields['field_client'] = $params['client'] ;
+               }
+               $fields['field_status'] = "unpayed" ;    
+               $items = [];
+               foreach($arrayData['items'] as $key => $item){    
+               // $article = \Drupal::service('entity_parser.manager')->node_parser( $item["id"]);
+               $items[$key] = [
+                  'field_article' => $item["id"],
+                  'field_quantite' => $item["qte"]
+               ];
+               }
+               $fields['field_articles'] = $items;
+               $com_new = \Drupal::service('crud')->save('node', 'commande', $fields);
+               if(is_object($com_new )){
+                  $service = \Drupal::service('drupal.helper');
+                  $nid = $service->helper->redirectTo("/frontdesk?new=".$com_new->id());
+                  return true;
+               }
+            } 
             return false ;
        
        }
